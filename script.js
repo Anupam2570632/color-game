885d04076eb8f6fccb3d4d5febab4d4a80358a11
Anupam2570632 <128147498+Anupam2570632@users.noreply.github.com>
2023-12-31 14:39:07 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,195 @@
+var btn = document.querySelectorAll("button")
+var color = document.querySelectorAll(".color")
+
+var red = 0
+btn[0].addEventListener("click",function() {
+    if(red == 0){
+        color[0].style.backgroundColor = "red"
+        red = 1
+    }
+    else {
+        color[0].style.backgroundColor = "white"
+        red = 0
+    }
+
+})
+
+var green = 0
+btn[1].addEventListener("click",function() {
+    if(green == 0){
+        color[1].style.backgroundColor = "green"
+        green = 1
+    }
+    else {
+        color[1].style.backgroundColor = "white"
+        green = 0
+    }
+})
+var blue = 0
+btn[2].addEventListener("click",function() {
+    if(blue == 0){
+        color[2].style.backgroundColor = "blue"
+        blue = 1
+    }
+    else {
+        color[2].style.backgroundColor = "white"
+        blue = 0
+    }
+})
+btn[3].addEventListener("click",function() {
+    color[3].style.backgroundColor = "white"
+})
+var black = 0
+btn[4].addEventListener("click",function() {
+    if(black == 0){
+        color[4].style.backgroundColor = "black"
+        black = 1
+    }
+    else {
+        color[4].style.backgroundColor = "white"
+        black = 0
+    }
+})
+var yellow = 0
+btn[5].addEventListener("click",function() {
+    if(yellow == 0){
+        color[5].style.backgroundColor = "yellow"
+        yellow = 1
+    }
+    else {
+        color[5].style.backgroundColor = "white"
+        yellow = 0
+    }
+})
+var pink = 0
+btn[6].addEventListener("click",function() {
+    if(pink == 0){
+        color[6].style.backgroundColor = "pink"
+        pink = 1
+    }
+    else {
+        color[6].style.backgroundColor = "white"
+        pink = 0
+    }
+})
+var purple = 0
+btn[7].addEventListener("click",function() {
+    if(purple == 0){
+        color[7].style.backgroundColor = "purple"
+        purple = 1
+    }
+    else {
+        color[7].style.backgroundColor = "white"
+        purple = 0
+    }
+})
+var orange = 0
+btn[8].addEventListener("click",function() {
+    if(orange == 0){
+        color[8].style.backgroundColor = "orange"
+        orange = 1
+    }
+    else {
+        color[8].style.backgroundColor = "white"
+        orange = 0
+    }
+})
+var megenta = 0
+btn[9].addEventListener("click",function() {
+    if(megenta == 0){
+        color[9].style.backgroundColor = "magenta"
+        megenta = 1
+    }
+    else {
+        color[9].style.backgroundColor = "white"
+        megenta = 0
+    }
+})
+var violet = 0
+btn[10].addEventListener("click",function() {
+    if(violet == 0){
+        color[10].style.backgroundColor = "violet"
+        violet = 1
+    }
+    else {
+        color[10].style.backgroundColor = "white"
+        violet = 0
+    }
+})
+var amber = 0
+btn[11].addEventListener("click",function() {
+    if(amber == 0){
+        color[11].style.backgroundColor = "coral"
+        amber = 1
+    }
+    else {
+        color[11].style.backgroundColor = "white"
+        amber = 0
+    }
+})
+var vermillion = 0
+btn[12].addEventListener("click",function() {
+    if(vermillion == 0){
+        color[12].style.backgroundColor = "gold"
+        vermillion = 1
+    }
+    else {
+        color[12].style.backgroundColor = "white"
+        vermillion = 0
+    }
+})
+var teal = 0
+btn[13].addEventListener("click",function() {
+    if(teal == 0){
+        color[13].style.backgroundColor = "indigo"
+        teal = 1
+    }
+    else {
+        color[13].style.backgroundColor = "white"
+        teal = 0
+    }
+})
+var gray = 0
+btn[14].addEventListener("click",function() {
+    if(gray == 0){
+        color[14].style.backgroundColor = "gray"
+        gray = 1
+    }
+    else {
+        color[14].style.backgroundColor = "white"
+        gray = 0
+    }
+})
+var brown = 0
+btn[15].addEventListener("click",function() {
+    if(brown == 0){
+        color[15].style.backgroundColor = "brown"
+        brown = 1
+    }
+    else {
+        color[15].style.backgroundColor = "white"
+        brown = 0
+    }
+})
+var violate = 0
+btn[16].addEventListener("click",function() {
+    if(violate == 0){
+        color[16].style.backgroundColor = "violet"
+        violate = 1
+    }
+    else {
+        color[16].style.backgroundColor = "white"
+        violate = 0
+    }
+})
+var maroon = 0
+btn[17].addEventListener("click",function() {
+    if(maroon == 0){
+        color[17].style.backgroundColor = "maroon"
+        maroon = 1
+    }
+    else {
+        color[17].style.backgroundColor = "white"
+        maroon = 0
+    }
+})
